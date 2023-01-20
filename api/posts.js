303b7d17bd942efdaf89c684c6e8a684463144ec
Wubbiez <108950491+Wubbiez@users.noreply.github.com>
2023-01-19 21:31:46 -0500

    Part 2 Finished, Part 3 Started

--- api/posts.js
+++ api/posts.js
@@ -1,11 +1,48 @@
 const express = require('express');
 const postsRouter = express.Router();
-const {getAllPosts} = require('../db')
+const {getAllPosts, createPost, getUserById} = require('../db');
+const {requireUser} = require('./utils');
+const jwt = require('jsonwebtoken');
+
+
 
 postsRouter.use((req, res, next) => {
   console.log("A request is being made to /posts");
 
   next();
+});
+
+postsRouter.post('/', requireUser, async (req,res,next) => {
+  const {title, content, tags = ""} = req.body;
+
+  const tagArr = tags.trim().split(/\s+/);
+  const postData = {};
+
+
+  if(tagArr.length) {
+    postData.tags = tagArr;
+  }
+
+  try {
+    postData.authorId = req.user.id;
+    postData.title = title;
+    postData.content = content;
+    const post = await createPost(postData);
+
+    if (post) {
+      res.send(post)
+    }
+
+    else {
+      next({
+        name: "CouldNotCreatePost",
+        message: "Please supply all information necessary for a post"
+      })
+    }
+  }
+   catch({name, message}) {
+    next({name, message});
+  }
 });
 
 postsRouter.get('/', async (req,res) => {
