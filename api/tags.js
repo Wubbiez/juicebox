5b51f781b1b33e791ce74d080247343124dfa9cd
Wubbiez <108950491+Wubbiez@users.noreply.github.com>
2023-01-22 13:47:45 -0500

    Finished

--- api/tags.js
+++ api/tags.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const tagsRouter = express.Router();
-const {getAllTags} = require('../db')
+const {getAllTags, getAllPosts, getPostsByTagName} = require('../db')
 
 tagsRouter.use((req, res, next) => {
   console.log("A request is being made to /tags");
 
   next();
+});
+
+tagsRouter.get('/:tagName/posts', async (req, res, next) => {
+  const { tagName } = req.params;
+  try {
+
+    const allPostsWithTag = await getPostsByTagName(tagName);
+
+    const userPosts = allPostsWithTag.filter(post => {
+      return post.active && (req.user && post.author.id === req.user.id);
+    });
+
+    res.send({userPosts})
+
+  } catch ({ name, message }) {
+    next({name,message})
+  }
 });
 
 tagsRouter.get('/', async (req,res) => {
